9d137e6c4f50ac936a856b12b02af95de872acc9
omiipatil <92794706+omiipatil@users.noreply.github.com>
2022-07-28 19:40:55 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+function handeldata(){
+var a= document.getElementById("data1").innerHTML="Table data";
+ 
+}
+
+function handelsign(){
+    var b=document.getElementById("data2").innerHTML="Thankufor Visit";
+}
